2053d54efc8900462bc4a58a4d9bbb95e056089a
Ecaterina Moraru (Valica) <valicac@gmail.com>
2014-12-06 20:03:22 +0200

    1: Compile LESS files with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    // compile less stylesheets to css -----------------------------------------
+    less: {
+      build: {
+        files: {
+          'dist/css/style.css': 'src/css/style.less'
+        }
+      }
+    },
+
+    // configure cssmin to minify css files ------------------------------------
+    cssmin: {
+      build: {
+        files: {
+          'dist/css/style.min.css': 'dist/css/style.css'
+        }
+      }
+    },
+
+    // configure watch to auto update ------------------------------------------ 
+    watch: { 
+      stylesheets: { 
+        files: ['src/**/*.css', 'src/**/*.less'], 
+        tasks: ['less', 'cssmin'] 
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', ['watch']);
+
+};
