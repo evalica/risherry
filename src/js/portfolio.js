4537d69c417054169232f7a03abc9345a8ec6d0a
Ecaterina Moraru (Valica) <valicac@gmail.com>
2015-01-07 04:11:48 +0200

    Issue #7: Using Behance API to pull portfolio

---
+++ src/js/portfolio.js
@@ -0,0 +1,24 @@
+var apiKey  = '83ALWlFcfphzIjCYB9q0lNx8ARptcRn9';
+var userID  = 'risherry';
+(function() {
+  var perPage = 10;
+  var behanceProjectAPI = 'http://www.behance.net/v2/users/'+ userID +'/projects?callback=?&api_key=' + apiKey + '&per_page=' + perPage;
+
+  function setPortfolioTemplate() {
+    var projectData = JSON.parse(sessionStorage.getItem('behanceProject')),
+      getTemplate = $('#portfolio-template').html(),
+      template    = Handlebars.compile(getTemplate),
+      result      = template(projectData);
+    $('#portfolio').html(result);
+  };
+
+  if(sessionStorage.getItem('behanceProject')) {
+    setPortfolioTemplate();
+  } else {
+    $.getJSON(behanceProjectAPI, function(project) {
+      var data = JSON.stringify(project);
+      sessionStorage.setItem('behanceProject', data);
+      setPortfolioTemplate();
+    });
+  };
+})();
