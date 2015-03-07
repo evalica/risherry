e5ec8467e6bb6197f363f92e8c0948e191584bfc
Ecaterina Moraru (Valica) <valicac@gmail.com>
2015-03-07 12:42:52 +0200

    Issue #13: Break Content into separate Categories
    - Removing Theme#2

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
 
     // themes list -------------------------------------------------------------
     swatch: {
-      bliss:{}, moss:{}, 2:{}, bordo:{}
+      bliss:{}, moss:{}, bordo:{}
     },
 
     // copying the style.less file ---------------------------------------------
