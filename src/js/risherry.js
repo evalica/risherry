e3314fa110a2864761f4364ac5cd194c846ee944
Ecaterina Moraru (Valica) <valicac@gmail.com>
2015-04-17 01:04:53 +0300

    Issue #8: 'More' functionality on Tasks
    - Adding a ‘perPage’ parameter

--- src/js/risherry.js
+++ src/js/risherry.js
@@ -49,6 +49,34 @@
       return value;
     });
 
+    Handlebars.registerHelper("ifCond",function(v1,operator,v2,options) {
+      switch (operator)
+      {
+        case "==":
+          return (v1==v2)?options.fn(this):options.inverse(this);
+        case "!=":
+          return (v1!=v2)?options.fn(this):options.inverse(this);
+        case "===":
+          return (v1===v2)?options.fn(this):options.inverse(this);
+        case "!==":
+          return (v1!==v2)?options.fn(this):options.inverse(this);
+        case "&&":
+          return (v1&&v2)?options.fn(this):options.inverse(this);
+        case "||":
+          return (v1||v2)?options.fn(this):options.inverse(this);
+        case "<":
+          return (v1<v2)?options.fn(this):options.inverse(this);
+        case "<=":
+          return (v1<=v2)?options.fn(this):options.inverse(this);
+        case ">":
+          return (v1>v2)?options.fn(this):options.inverse(this);
+        case ">=":
+         return (v1>=v2)?options.fn(this):options.inverse(this);
+        default:
+          return eval(""+v1+operator+v2)?options.fn(this):options.inverse(this);
+      }
+    });
+
     function renderTemplateFromData(data, templateId) {
       var templateString = $(templateId).html(),
           template    = Handlebars.compile(templateString),
